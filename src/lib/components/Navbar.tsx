adb7d995813b94feb585acdcb335e4eeb522d8de
Andrew Manzanero <andrewmanzanero@proton.me>
2022-11-07 21:54:43 -0800

    whoops

--- src/lib/components/Navbar.tsx
+++ src/lib/components/Navbar.tsx
@@ -4,14 +4,14 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <Link href="/src/pages" className="btn-ghost btn text-xl normal-case">
+        <Link href="/" className="btn-ghost btn text-xl normal-case">
           KrisKringle
         </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal p-0">
           <li>
-            <Link href="/src/pages/home">Home</Link>
+            <Link href="/home">Home</Link>
           </li>
           <li>
             <Link href="/signup">Signup</Link>
